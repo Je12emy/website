0eae7e9ff76a38f0e1635f7c4cd2bbebbf9d9e0b
Jeremy Zelaya <jeremyzelaya@hotmail.es>
2024-04-15 01:25:13 -0600

    feat(backend): bind to port based on env var

--- main.go
+++ main.go
@@ -1,17 +1,24 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
 	mux := http.NewServeMux()
 	fs := http.FileServer(http.Dir("./public"))
 	// mux.Handle("/public", http.StripPrefix("/public/", fs))
 	mux.Handle("/", fs)
-	log.Print("Listening on port :8080")
-	err := http.ListenAndServe(":8080", mux)
+	log.Printf("Listening on port :%s", port)
+	err := http.ListenAndServe(fmt.Sprintf(":%s", port), mux)
 	if err != nil {
 		log.Fatal(err)
 	}
